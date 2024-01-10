fa22243a1f12ee851e16bc13ef23044372da979e
Shurahbeel Peerzada <108054504+justuravgcoder@users.noreply.github.com>
2024-01-10 20:47:36 +0000

    updated things

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/book.jpg";
 
 const imageAltText = "desktop with books and laptop";
 
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Control of mouse now on your fingertips",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "With the power of computer vision this project will allow you to navigate on your screen using your hands and a webcam",
+    url: "https://github.com/justuravgcoder/Virtual-Mouse",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Check out some of my dashboards",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "PowerBI and a bit of creativity thats all",
+    url: "https://github.com/justuravgcoder/PowerBI-Dashboard",
   },
   {
-    title: "My Resume Site",
+    title: "Check out my resume",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
+      "Coded in latex viewed in pdf",
     url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "Mango classifier app",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "When life gives you mangoes use this application of mine",
+    url: "https://github.com/justuravgcoder/flutter-app-mango-classifier/tree/master",
   },
 ];
 
